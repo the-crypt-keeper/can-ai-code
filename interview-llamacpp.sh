9091efbb21321b2808a1e6a85728614040e14a2a
Mike <mike@cannstandard.om>
2023-05-25 20:34:21 -0400

    clean up llama cpp interview example, add manticore/wizardmega prompt

--- interview-llamacpp.sh
+++ interview-llamacpp.sh
@@ -1,27 +1,18 @@
 #!/bin/bash
 
-#CRD716/ggml-vicuna-1.1-quantized
-#https://huggingface.co/TheBloke/MPT-7B-Instruct-GGML
-#https://huggingface.co/TheBloke/Wizard-Vicuna-13B-Uncensored-GGML
+MODEL="${MODEL:-/home/miner/ai/models/v3/Manticore-13B.ggmlv3.q5_0.bin}"
+INTERVIEW="${INTERVIEW:-python.csv}"
+PROMPT="${PROMPT:-prompts/Manticore.txt}"
+OUTPUT="${OUTPUT:-results/manticore-13b/}"
 
-#https://huggingface.co/TheBloke/wizardLM-7B-GGML
-#https://huggingface.co/TheBloke/WizardLM-7B-uncensored-GGML
-
-INTERVIEW=python.csv
-
-#PROMPT=prompts/Wizard-Vicuna.txt
-#MODEL="/home/miner/ai/models/v3/Wizard-Vicuna-13B-Uncensored.ggmlv3.q5_0.bin"
-#OUTPUT="results/wizard-vicuna/"
-
-PROMPT=prompts/Vicuna-1p1.txt
-MODEL="/home/miner/ai/models/v3/ggml-vicuna-7b-1.1-q5_0.bin"
-OUTPUT="results/vicuna-1.1-7b/"
+echo "INTERVIEW = $INTERVIEW"
+echo "MODEL = $MODEL, PROMPT = $PROMPT"
+echo "OUTPUT = $OUTPUT"
 
 SSH=miner
 MAIN="~/ai/latest/main"
 ARGS="--threads 4 --ctx_size 2048"
 PARAMS="--n_predict 512 --temp 0.7 --top_k 40 --top_p 0.1 --repeat_last_n 256 --batch_size 1024 --repeat_penalty 1.17647"
-
 
 mkdir -p $OUTPUT
 
@@ -35,6 +26,12 @@ while IFS= read -r line; do
 
     # Split the line by comma
     IFS="," read -r col1 col2 <<< "$line"
+
+    # Skip existing files
+    if [[ -e "${OUTPUT}${col1}.txt" ]]; then
+        echo "${col1} already done"
+        continue
+    fi
 
     # Remove leading/trailing quotes from the second column
     col2=${col2%\"}
