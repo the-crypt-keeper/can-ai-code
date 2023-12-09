61c78d9f662cde2c2102ebf34708c5d52b7c85f6
Mike <set>
2023-12-09 10:23:01 -0500

    Evaluate Open AI GPT4? (or ChatGPT 4) #121
    Re-run the full suite of currently available openai models

--- interview-langchain.py
+++ interview-langchain.py
@@ -1,35 +1,23 @@
 #!/usr/bin/env python3
-from langchain import LLMChain, PromptTemplate
 import argparse
 import json
 from time import sleep
 from prepare import save_interview
-from langchain.llms import ChatLiteLLM
+from langchain.chat_models import ChatLiteLLM
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
 
 def init_model(model, params):
-    # LangChain did not bother to standardize the names of any of the parameters,
-    # or even how to interact with them.  This is a hack to make things consistent.
-
     # integrating liteLLM to provide a standard I/O interface for every LLM
+    # see https://docs.litellm.ai/docs/providers for list of supported providers
     model_params = {
             'temperature': params['temperature'],
             'max_tokens': params['max_new_tokens'],
             'top_p': params['top_p'],
-            'presence_penalty': params['repetition_penalty']
+            'presence_penalty': params.get('repetition_penalty', 1.0)
 
     }
-    if model == 'ai21/j2-jumbo-instruct':
-        model_name = "j2-jumbo-instruct"
-    elif model == 'openai/chatgpt':
-        model_name = "gpt-3.5-turbo"
-    elif model == 'openai/gpt4':
-        model_name = "gpt-4"
-    elif model == 'cohere/command-nightly':
-        model_name = "command-nightly"
-    # [TODO] Add replicate, hugging face, VertexAI, see https://docs.litellm.ai/docs/completion/supported
-    else:
-        raise Exception('Unsupported model/provider')
-    return model_params, ChatLiteLLM(model=model_name, **model_params)
+    return model_params, ChatLiteLLM(model=model, **model_params)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Interview executor for LangChain')
