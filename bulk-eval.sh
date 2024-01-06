5cc48690d72bdc8ef4d42d9902e2bfe6d97dfcb9
Mike <set>
2024-01-05 22:26:18 -0500

    INTERVIEW and ALWAYS features

--- bulk-eval.sh
+++ bulk-eval.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+if [ "$INTERVIEW" == "" ]; then
+    INTERVIEW="junior-v2"
+fi
+
 if [ "$1" == "" ]; then
-    GLOB="results/interview_junior*.ndjson"
+    GLOB="results/interview_${INTERVIEW}*.ndjson"
 else
-    GLOB="$1/interview_junior*.ndjson"
+    GLOB="$1/interview_${INTERVIEW}*.ndjson"
 fi
 
 # Search for files matching "results/interview_junior-dev*.ndjson"
@@ -12,11 +16,14 @@ for file in $GLOB; do
 
     # Extract the corresponding eval file name
     eval_file="${file/interview/eval}"
+    if [ "$ALWAYS" == "1" ]; then
+      eval_file=""
+    fi
 
     # Check if the corresponding eval file exists
     if [ ! -f "$eval_file" ]; then
         # Execute ./evaluation.py with the input filename
-        ./evaluate.py --input "$file"
+        ./evaluate.py --interview ${INTERVIEW} --input "$file"
     else
         echo "Already evaluated $file"
     fi
